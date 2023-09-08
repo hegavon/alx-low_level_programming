0782bb83e7429af6ede74e25d25ef778ce3cb22d
hegavon <victorgavon@gmail.com>
2023-09-07 21:06:31 -0700

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the body of the code
+ *
+ * Return: outputs 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
