21c7487bfd48db3694ad86deeaf6d041e67f48e6
hegavon <victorgavon@gmail.com>
2023-10-23 12:47:53 -0700

    More Singly linked lists Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list and sets the head to NULL.
- * @head: A pointer to a pointer to the head of the list.
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *fresh_node = *head;
+	listint_t *temp;
 
-	for (; fresh_node != NULL;)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		listint_t *temp = fresh_node->next;
-
-		free(fresh_node);
-		fresh_node = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
 }
+
