b10545ea0608fa1f67423b74447bc8171827edc0
hegavon <victorgavon@gmail.com>
2023-10-26 15:47:09 -0700

    Bit Manipulation Task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /* Binary Conversion Functions */
 unsigned int binary_to_uint(const char *b);
@@ -11,9 +11,14 @@ void print_binary(unsigned long int n);
 /* Bit Manipulation Functions */
 int get_bit(unsigned long int n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 /* Endianness Detection Function */
 int get_endianness(void);
 
+/* Custom _putchar function */
+int _putchar(char c);
+
 #endif /* MAIN_H */
+
