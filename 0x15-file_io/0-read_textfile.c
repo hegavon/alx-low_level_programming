588236e6a416b77f91db4b6dd1afdf2aa082ca57
hegavon <victorgavon@gmail.com>
2023-10-30 15:53:41 -0700

    File I/O Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * @filename: File to be modified
+ * @letters: The letters
+ * Return: Always success
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	ssize_t opn, rd;
+
+	char *ptr;
+
+	if (filename == NULL)
+		return (0);
+	ptr = malloc(sizeof(char) * letters);
+	if (ptr == NULL)
+		return (0);
+
+	opn = open(filename, O_RDONLY);
+	rd = read(opn, ptr, letters);
+	wr = write(STDOUT_FILENO, ptr, rd);
+	if (opn == -1 || rd == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+	free(ptr);
+	close(opn);
+	return (0);
+}
